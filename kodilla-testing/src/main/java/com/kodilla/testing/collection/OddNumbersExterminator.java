302206b87aa34899974f95cf8563ed8a0c568cdf
lukasz <lukaszl95@o2.pl>
2020-01-09 10:02:32 +0100

    'Zadanie3'

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -1,17 +1,17 @@
 package com.kodilla.testing.collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class OddNumbersExterminator {
-    private ArrayList<Integer> numbers;
 
-    public ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
+    public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> number = new ArrayList<Integer>();
         for (int n=0; n < numbers.size(); n++){
-            if(numbers.get(n) % 2 > 0){
-                numbers.remove(n);
-                n--;
+            if(numbers.get(n) % 2 == 0){
+                number.add(numbers.get(n));
             }
         }
-        return numbers;
+        return number;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,9 +1,9 @@
 package com.kodilla.testing.collection;
 import com.kodilla.testing.collection.OddNumbersExterminator;
 import org.junit.*;
-import sun.invoke.empty.Empty;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class CollectionTestSuite {
     @Before
@@ -17,24 +17,24 @@ public class CollectionTestSuite {
     @Test
     public void testOddNumbersExterminatorNormalList (){
         OddNumbersExterminator simpleUser = new OddNumbersExterminator();
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        List<Integer> numbers = new ArrayList<Integer>();
 
         for(int n=0; n < 10; n++){
             int random = (int)(Math.random() * 6 + 1);
             numbers.add(random);
         };
 
-        System.out.println("Odd " + numbers);
-        ArrayList<Integer> result = simpleUser.exterminate(numbers);
-        System.out.println("Odd " + result);
+        System.out.println("Normal List " + numbers);
+        List<Integer> result = simpleUser.exterminate(numbers);
+        System.out.println("Normal List " + result);
     }
     @Test
     public void testOddNumbersExterminatorEmptyList (){
         OddNumbersExterminator simpleUser = new OddNumbersExterminator();
-        ArrayList<Integer> numbers2 = new ArrayList<Integer>();
+        List<Integer> numbers2 = new ArrayList<Integer>();
 
-        System.out.println("Empty " + numbers2);
-        ArrayList<Integer> result2 = simpleUser.exterminate(numbers2);
-        System.out.println("Empty " + result2);
+        System.out.println("Empty List " + numbers2);
+        List<Integer> result2 = simpleUser.exterminate(numbers2);
+        System.out.println("Empty List " + result2);
     }
 }
