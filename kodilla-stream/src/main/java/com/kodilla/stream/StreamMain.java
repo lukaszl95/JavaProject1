7b343d9dee1aaa96e0b6b2e2ccec462a8a05bb20
lukasz <lukaszl95@o2.pl>
2020-02-29 14:07:54 +0100

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,14 +1,22 @@
 package com.kodilla.stream;
 
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.immutable.ForumUser;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        //Zadanie 7.1
+        /*PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         System.out.println(poemBeautifier.beautify("słowo", (word) -> word.toUpperCase()));
         System.out.println(poemBeautifier.beautify("słowo", (word) -> "*" + word + "*"));
-        System.out.println(poemBeautifier.beautify("słowo", (word) -> word + " <-> " + word));
+        System.out.println(poemBeautifier.beautify("słowo", (word) -> word + " <-> " + word));*/
+        //Zadanie 7.2
+        //System.out.println("Using Stream to generate even numbers from 1 to 20");
+       //NumbersGenerator.generateEven(20);
+        ForumUser forumUser = new ForumUser("Adam123", "Adam");
+        System.out.println("User name: " + forumUser.getUsername());
+        System.out.println("Real name: " + forumUser.getRealName());
     }
 }
